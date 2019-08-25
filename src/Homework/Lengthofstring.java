e5c8cc3d05c8bea218cbf6661c09151b3dd320fb
faizansyed1008 <faizansyed1008@gmail.com>
2019-08-25 16:36:26 -0400

    New class work

--- src/Homework/Lengthofstring.java
+++ src/Homework/Lengthofstring.java
@@ -3,23 +3,40 @@ package Homework;
 public class Lengthofstring {
 
 	public static void main(String[] args) {
-
-		String name = "Faizan";
-		int length = name.length();
-		
-			
-		if(name.length()== 5) {
-			System.out.println("The String is not Empty and length is : " + name.length());
-			
-		}else if(name.length()<5) {
-			System.out.println("");
-			
-		}else if ( name.length()>5) {
-			System.out.println("The String is Empty and length is : " + name.length());
-			
-		}
-		
+		String name = faizanCode("pizza", 5);
+		System.out.println(name);
 
 	}
 
+	public static String faizanCode(String str, int length) {
+
+		String newstr = null;
+
+		if (str.length() == length) {
+
+			newstr = str;
+
+		} else if (str.length() > length) {
+
+			newstr = str.substring(str.length() - length);
+
+		} else if (str.length() < length) {
+			int difference = length - str.length();
+
+			System.out.println("I need to loop <" + difference + ">time");
+
+			while (difference >= 0) {
+				str = str.concat("n");
+				if (str.length() == length) {
+					break;
+				}
+
+				difference--;
+			}
+			newstr = str;
+
+		}
+		return newstr;
+
+	}
 }
---
+++ src/ourFirstJavaProgram/Method.java
@@ -0,0 +1,49 @@
+package ourFirstJavaProgram;
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+		maxfunction(3, 5);
+		maxfunction(5,100);
+		maxfunction(6,200);
+		
+		
+		
+		
+		// min = (n1 > n2) ? n2 : n1;
+		// boolean true:false
+
+		// System.out.println(min);
+
+//public static int minFunction(int n1, int n2) {
+
+//
+//		int min;
+//
+//		
+//			if (n1 > n2)
+//				min = n2;
+//			else
+//				min = n1;
+//
+//			return min;
+
+	}
+
+	public static void maxfunction(int n1, int n2) {
+
+		
+		int max;
+
+		if (n1 < n2)
+			max = n2;
+		else
+			max = n1;
+		
+	System.out.println(max);
+	
+
+	}
+
+}
